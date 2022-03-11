be7a45d3884a71c450a71118272a573655f12270
Baros <776638@dpsk12.org>
2022-03-11 14:34:49 -0700

    Aidan

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -4,12 +4,33 @@ SEPARATOR = " "
 
 cases = int(sys.stdin.readline().rstrip())
 
+
+def close(y_pos, x_pos, arr):
+    for i in range(-1,2):
+        for j in range(-1,2):
+            if y_pos+i >= 0 and y_pos+i < rows and x_pos+j >= 0 and x_pos+j < cols:
+                if arr[y_pos+i][x_pos+j] != "*":
+                    arr[y_pos+i][x_pos+j] += 1
+    return arr
+
 for caseNum in range(cases):
 
-    # Read in the next line and split as 2 float values speed and distance
+    grid = []
     line = sys.stdin.readline().rstrip()
-    x, y = (float(val) for val in line.split(SEPARATOR))
+    rows, cols, bombs = (int(val) for val in line.split(SEPARATOR))
 
-    print(int(x+y), end = "")
-    print(" ", end = "")
-    print(int(x*y))
+    grid = [[0 for _ in range(cols)]for _ in range(rows)]
+    for bomb in range(bombs):
+        line = sys.stdin.readline().rstrip()
+        y, x = (int(val) for val in line.split(SEPARATOR))
+        for i in range(rows):
+            for j in range(cols):
+                if y == i and x == j:
+                    grid[i][j] = "*"
+                    grid = close(i, j, grid)
+
+    for i in range(rows):
+        for j in range(cols):
+            print(grid[i][j], end = "")
+        print()
+        
